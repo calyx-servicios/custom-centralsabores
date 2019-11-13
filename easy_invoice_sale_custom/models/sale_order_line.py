67b418e5d4c7e5057d8fbc8bd03adf88d3b7b9c1
Odzomek Jorge Matias <odzomek.matias@gmail.com>
2019-11-13 09:22:50 +0000

    [FIX] se arregla la variable de productos en sale.order.-

--- easy_invoice_sale_custom/models/sale_order_line.py
+++ easy_invoice_sale_custom/models/sale_order_line.py
@@ -9,10 +9,11 @@ class SaleOrderLine(models.Model):
 
 
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist') 
-    pricelist_product_list_ids = fields.Many2many('product.template', 'product_template_pricelist_sale_line_rel', 'product_id', 
+    pricelist_product_list_ids = fields.Many2many('product.product', 'product_template_pricelist_sale_line_rel', 'product_id', 
             'pricelist_id', string='Listo Product',compute='_compute_pricelist_product_list')
     
 ### end Fields
+    
     
     @api.multi
     @api.depends('pricelist_id')
@@ -21,5 +22,7 @@ class SaleOrderLine(models.Model):
             pricelist_product_list_ids =[]
             for line_obj in rec.pricelist_id.item_ids:
                 if line_obj.applied_on == '1_product':  
-                    pricelist_product_list_ids.append(line_obj.product_tmpl_id.id)
+                    self_ids = self.env['product.product'].search([ ('product_tmpl_id', '=', line_obj.product_tmpl_id.id)])
+                    if self_ids:
+                        pricelist_product_list_ids.append(self_ids[0].id)
             rec.pricelist_product_list_ids = pricelist_product_list_ids
