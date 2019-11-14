7cce0578877ce996c43d0b5bdb220a0fc203d9c3
Odzomek Jorge Matias <odzomek.matias@gmail.com>
2019-11-14 20:05:21 +0000

    [UPD] se agrega onchange de linea de factura.-

--- easy_invoice_sale_custom/models/easy_invoice_line.py
+++ easy_invoice_sale_custom/models/easy_invoice_line.py
@@ -9,8 +9,8 @@ class EasyInvoiceLine(models.Model):
 
 
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist') 
-    pricelist_product_list_ids = fields.Many2many('product.template', 'product_template_pricelist_sale_line_rel', 'product_id', 
-            'pricelist_id', string='Listo Product',compute='_compute_pricelist_product_list')
+    pricelist_product_list_ids = fields.Many2many('product.product', 'product_template_pricelist_sale_line_rel', 'product_id', 
+            'pricelist_id', string='List Product',compute='_compute_pricelist_product_list')
     
 ### end Fields
     
@@ -21,5 +21,23 @@ class EasyInvoiceLine(models.Model):
             pricelist_product_list_ids =[]
             for line_obj in rec.pricelist_id.item_ids:
                 if line_obj.applied_on == '1_product':  
-                    pricelist_product_list_ids.append(line_obj.product_tmpl_id.id)
+                    self_ids = self.env['product.product'].search([ ('product_tmpl_id', '=', line_obj.product_tmpl_id.id)])
+                    if self_ids:
+                        pricelist_product_list_ids.append(self_ids[0].id)
             rec.pricelist_product_list_ids = pricelist_product_list_ids
+
+
+
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        for rec in self:
+            if rec.pricelist_id:
+                for line_obj in rec.pricelist_id.item_ids:
+                    if line_obj.applied_on == '1_product':
+                        self_ids = rec.env['product.product'].search([ ('product_tmpl_id', '=', line_obj.product_tmpl_id.id)])
+                        if self_ids:
+                            if self_ids[0].id == rec.product_id.id:
+                                rec.price_unit = line_obj.fixed_price
+
+
+        #self.price = self.product_id.list_price or 0
