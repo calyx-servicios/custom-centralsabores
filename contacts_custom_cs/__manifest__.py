cc961c5ce7133ed3b3ed0a448ddb0e061b752c22
mariodmoreno <mariodmoreno@gmail.com>
2019-12-19 15:41:17 -0300

    [ADD] [contacts_custom_cs]

---
+++ contacts_custom_cs/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contacts Custom CS",
+    'summary': """
+        Contacts Custom CS""",
+    'description': """
+        
+    """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",
+    'category': 'Tools',
+    'version': '0.1',
+    'depends': ['base', 'contacts', 'l10n_ar_partner'],
+
+    # always loaded
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+
+    'demo': [
+    ],
+}
