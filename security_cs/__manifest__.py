753d09f806abecdd7f7f330f4cf07167dfa26795
mariodmoreno <mariodmorenosoltic@gmail.com>
2020-01-01 18:25:27 -0300

    [UPD]

--- security_cs/__manifest__.py
+++ security_cs/__manifest__.py
@@ -10,12 +10,13 @@
     'website': "http://www.calyxservicios.com.ar",
     'category': 'Tools',
     'version': '0.1',
-    'depends': ['base', 'contacts', 'l10n_ar_partner', 'easy_invoice', 'sale'],
+    'depends': ['base', 'contacts', 'l10n_ar_partner', 'easy_invoice', 'sale', 'easy_invoice_sale', 'sale_management'],
 
     # always loaded
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
+        'views/sale_view.xml',
     ],
 
     'demo': [
