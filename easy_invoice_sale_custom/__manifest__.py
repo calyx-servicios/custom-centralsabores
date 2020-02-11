c1baf576643a24190c7913e05c595139b1389b70
Odzomek Jorge Matias <odzomek.matias@gmail.com>
2020-02-11 19:23:50 +0000

    [UPD] issue de velocidad.-

--- easy_invoice_sale_custom/__manifest__.py
+++ easy_invoice_sale_custom/__manifest__.py
@@ -26,7 +26,7 @@
         ],
     'data': [
         'views/sale_order_view.xml',
-        'views/easy_invoice_view.xml',
+        #'views/easy_invoice_view.xml',
         'views/res_partner_view.xml',
         'views/easy_payment_group_view.xml',
     ],
--- easy_invoice_sale_custom/models/easy_invoice_line.py
+++ easy_invoice_sale_custom/models/easy_invoice_line.py
@@ -5,39 +5,16 @@ from odoo.exceptions import ValidationError
 
 
 class EasyInvoiceLine(models.Model):
+    
     _inherit = "easy.invoice.line"
 
-
-    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist') 
-    pricelist_product_list_ids = fields.Many2many('product.product', 'product_template_pricelist_sale_line_rel', 'product_id', 
-            'pricelist_id', string='List Product',compute='_compute_pricelist_product_list')
-    
-### end Fields
-    
-    @api.multi
-    @api.depends('pricelist_id')
-    def _compute_pricelist_product_list(self):
-        for rec in self:
-            pricelist_product_list_ids =[]
-            for line_obj in rec.pricelist_id.item_ids:
-                if line_obj.applied_on == '1_product':  
-                    self_ids = self.env['product.product'].search([ ('product_tmpl_id', '=', line_obj.product_tmpl_id.id)])
-                    if self_ids:
-                        pricelist_product_list_ids.append(self_ids[0].id)
-            rec.pricelist_product_list_ids = pricelist_product_list_ids
-
-
-
     @api.onchange('product_id')
-    def _onchange_product_id2(self):
+    def _onchange_product_id22(self):
         for rec in self:
-            if rec.pricelist_id:
-                for line_obj in rec.pricelist_id.item_ids:
+            if rec.invoice_id.type in ('out_invoice','out_refund') and rec.invoice_id.partner_id.default_product_pricelist_id:
+                for line_obj in rec.invoice_id.partner_id.default_product_pricelist_id.item_ids:
                     if line_obj.applied_on == '1_product':
                         self_ids = rec.env['product.product'].search([ ('product_tmpl_id', '=', line_obj.product_tmpl_id.id)])
                         if self_ids:
                             if self_ids[0].id == rec.product_id.id:
                                 rec.price_unit = line_obj.fixed_price
-
-
-        #self.price = self.product_id.list_price or 0
