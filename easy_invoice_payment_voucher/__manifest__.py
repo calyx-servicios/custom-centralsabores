379090e2949c44aec2f1bd3c99a2789b7f21b7d1
Agustin Wisky <agustinwisky@MacBook-de-Agustin.local>
2020-03-06 10:02:07 -0300

    [NEW][easy_invoice_payment_voucher]

---
+++ easy_invoice_payment_voucher/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Easy Invoice Paymnet Boucher",
+    'summary': """
+        Easy Invoice Payment Boucher""",
+
+    'description': """
+
+    """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",
+    'category': 'Easy Invoice',
+    'version': '11.0.1.0.0',
+    'depends' : [
+        'base',
+        'easy_invoice',
+        ],
+    'data': [
+        'report/payment_group_report.xml',
+        'report/payment_group_template.xml',
+    ],
+}
