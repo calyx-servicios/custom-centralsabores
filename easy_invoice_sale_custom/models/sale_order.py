5adb9e3edbd9cae4a3e8adc2e6a1e00d65a62d61
Milton Guzman <miltonguzmanf@gmail.com>
2021-02-10 22:45:04 -0300

    [FIX][easy_invoice_sale_custom] Repair inherit method
     Incidence nbr./s.: ODOO-619.
    
     - Change method _prepare_line_easy_invoice in model sale.order.line

--- easy_invoice_sale_custom/models/sale_order.py
+++ easy_invoice_sale_custom/models/sale_order.py
@@ -32,25 +32,16 @@ class SaleOrder(models.Model):
             invoice_tuple['context']['invoice_obj'].confirm()
         return self.env['ir.actions.report']._get_report_from_name('easy_invoice.report_easy_invoice').report_action(invoice_tuple['context']['invoice_obj'])
 
-    @api.multi
-    def _create_line_easy_invoice(self,invoice_created):
-        for line_obj in self.order_line:
-            vals = {
-                'invoice_id': invoice_created.id,
-                'name': line_obj.name ,
-                'price_unit': line_obj.price_unit ,
-                'uom_id':    line_obj.product_uom.id,
-                'product_id': line_obj.product_id.id,
-                'quantity': line_obj.product_uom_qty,
-                'currency_id': line_obj.currency_id.id,
-                'company_id': line_obj.company_id.id,
-                'unit_detail': line_obj.unit_detail,
-            }
-            
-            invoice_line_created = self.env['easy.invoice.line'].create(vals)
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     unit_detail = fields.Float('Detalle Unidad', digits=(16,2))
-
+    
+    def _prepare_line_easy_invoice(self, invoice_created):
+        """We add the unit_detail field to the dictionary of values ​​
+           so that it loads it in the easy invoice """
+        res = super(SaleOrderLine, self)._prepare_line_easy_invoice(invoice_created)
+        res['unit_detail'] = self.unit_detail
+        
+        return res
