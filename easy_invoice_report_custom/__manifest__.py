c7bd18802d88f25d3ed64368f74d2a3d969ad053
DarwinAndrade <andres.63.d@gmail.com>
2022-05-04 18:22:13 -0300

    [ADD][easy_invoice_report_custom] Incidence nbr./s.: ODOO-3694.  - changes in the report I send

---
+++ easy_invoice_report_custom/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Easy Invoice Report Custom',
+    'version': '11.0.1',
+    'category': 'Tools',
+    'author': "Calyx",
+    'maintainers': ['DarwinAndrade'],
+    'website': 'www.calyxservicios.com.ar',
+    'license': 'AGPL-3',
+    'summary': '''changes in the report I send''',
+    'depends': [
+        'stock', 'easy_invoice', 'sale'
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'reports/invoice_templatet.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
