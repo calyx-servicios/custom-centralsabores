82d3f91de4e75aadecee1d57413b1c75e5a46772
DarwinAndrade <andres.63.d@gmail.com>
2022-05-05 15:15:52 -0300

    upd

--- easy_invoice_report_custom/models/easy_invoice.py
+++ easy_invoice_report_custom/models/easy_invoice.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class EasyInvoiceLine(models.Model):
     _inherit = "easy.invoice.line"
 
-    product_uom_qty = fields.Float(string='invoiced amount', store=True)
-    delivered_qty = fields.Float(string='delivered quantity', store=True)
+    product_uom_qty = fields.Float(string='invoiced amount')
+    delivered_qty = fields.Float(string='delivered quantity')
