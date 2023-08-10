44803091311e34cf004cfd1903d4fc0afb42b289
gpperez <gpperez@gmail.com>
2023-08-10 17:30:22 -0300

    module updates

--- product_pricelist_permision_custom/models/product_template.py
+++ product_pricelist_permision_custom/models/product_template.py
@@ -1,24 +1,30 @@
-from odoo import fields, models
+from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @api.model
+    def create(self, vals):
+        if 'item_ids' in vals:
+            for pricelist in vals['item_ids']:
+                for other_pricelist in vals['item_ids']:
+                    if other_pricelist[1] != pricelist[1] and other_pricelist[2]['pricelist_id'] == pricelist[2]['pricelist_id']:
+                        raise UserError(('You cannot enter the same price list more than once.'))
+
+        return super(ProductTemplate, self).create(vals)
+
     def write(self, vals):
-        #que no se puedan ingresar 2 veces la misma lista de precios en el producto
+        #You cannot enter the same price list more than once.
         if 'item_ids' in vals:
             for pricelist in vals['item_ids']:
                 if pricelist[0] == 0:
                     for current_pricelist in self.item_ids:
                         if current_pricelist.pricelist_id.id == pricelist[2]['pricelist_id']:
-                            raise UserError(('No puede ingresar la misma lista de precios mas de una vez.'))
+                            raise UserError(('You cannot enter the same price list more than once.'))
                     for new_pricelist in vals['item_ids']:
                         if new_pricelist[0] == 0:
                             if new_pricelist[1] != pricelist[1] and new_pricelist[2]['pricelist_id'] == pricelist[2]['pricelist_id']:
-                                raise UserError(('No puede ingresar la misma lista de precios mas de una vez.'))
+                                raise UserError(('You cannot enter the same price list more than once.'))
 
         super(ProductTemplate, self).write(vals)
-
-    def chek_pricelist_duplicated(self):
-
-        return False
