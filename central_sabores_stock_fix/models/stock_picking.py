ddb26b87e419f94ee7cb203091395a77a3887151
Andrés Shoobridge <shoobridgea@gmail.com>
2026-03-20 11:18:05 -0300

    Multiples opciones para eliminar esas lineas problematicas

--- central_sabores_stock_fix/models/stock_picking.py
+++ central_sabores_stock_fix/models/stock_picking.py
@@ -1,58 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from odoo import _, api, models
+from odoo import fields
 from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+    force_unlink_enabled = fields.Boolean(
+        string="Forzado de lineas activo",
+        default=False,
+        copy=False,
+    )
 
     @api.multi
-    def action_suppress_moves_force(self):
-        reusable_delete_model = self.env["reusable.sql.delete"].sudo()
-        deleted_move_ids = []
-        failed_picking_ids = []
+    def action_enable_force_unlink(self):
+        self.write({"force_unlink_enabled": True})
+        return {"type": "ir.actions.client", "tag": "reload"}
 
-        for picking in self:
-            if picking.state not in ("waiting", "assigned"):
-                failed_picking_ids.append(picking.id)
-                continue
-
-            move_ids = picking.move_lines.ids
-            if not move_ids:
-                continue
-
-            try:
-                reusable_delete_model.delete_records("stock.move", "picking_id", picking.id)
-            except UserError as err:
-                message = str(err)
-                if "Se eliminaron los siguientes IDs" in message:
-                    deleted_move_ids.extend(move_ids)
-                elif "No se encontraron registros" in message:
-                    continue
-                else:
-                    failed_picking_ids.append(picking.id)
-            except Exception:
-                failed_picking_ids.append(picking.id)
-
-        if failed_picking_ids:
-            raise UserError(
-                _("No se pudo suprimir moves. Modelo: stock.picking, ID: %s")
-                % ", ".join(map(str, failed_picking_ids))
-            )
-
-        if deleted_move_ids:
-            raise UserError(
-                _("Registros suprimidos. Modelo: stock.move, ID: %s")
-                % ", ".join(map(str, deleted_move_ids))
-            )
-
-        raise UserError(_("No se encontraron moves para suprimir."))
+    @api.multi
+    def action_disable_force_unlink(self):
+        self.write({"force_unlink_enabled": False})
+        return {"type": "ir.actions.client", "tag": "reload"}
 
     @api.multi
     def action_suppress_force(self):
-        reusable_delete_model = self.env["reusable.sql.delete"].sudo()
-        deleted_picking_ids = []
+        deleted_ids = []
         failed_ids = []
 
         for picking in self:
@@ -61,13 +34,19 @@ class StockPicking(models.Model):
                 continue
 
             try:
-                reusable_delete_model.delete_records("stock.picking", "id", picking.id)
-            except UserError as err:
-                # El modulo reutilizable usa UserError tambien para informar exito.
-                if "Se eliminaron los siguientes IDs" in str(err):
-                    deleted_picking_ids.append(picking.id)
-                else:
-                    failed_ids.append(picking.id)
+                # Suprimir completo: lineas, moves y picking.
+                self.env.cr.execute(
+                    "UPDATE stock_move_line SET picking_id = NULL, move_id = NULL WHERE picking_id = %s",
+                    (picking.id,),
+                )
+                self.env.cr.execute("SET session_replication_role = 'replica';")
+                try:
+                    self.env.cr.execute("DELETE FROM stock_move_line WHERE picking_id = %s", (picking.id,))
+                    self.env.cr.execute("DELETE FROM stock_move WHERE picking_id = %s", (picking.id,))
+                    self.env.cr.execute("DELETE FROM stock_picking WHERE id = %s", (picking.id,))
+                finally:
+                    self.env.cr.execute("SET session_replication_role = 'origin';")
+                deleted_ids.append(picking.id)
             except Exception:
                 failed_ids.append(picking.id)
 
@@ -77,10 +56,7 @@ class StockPicking(models.Model):
                 % ", ".join(map(str, failed_ids))
             )
 
-        if deleted_picking_ids:
-            raise UserError(
-                _("Registros suprimidos. Modelo: stock.picking, ID: %s")
-                % ", ".join(map(str, deleted_picking_ids))
-            )
+        if deleted_ids:
+            return {"type": "ir.actions.client", "tag": "reload"}
 
-        return True
+        return {"type": "ir.actions.client", "tag": "reload"}
